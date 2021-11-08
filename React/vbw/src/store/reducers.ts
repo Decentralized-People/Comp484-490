6789f9858bdd555255c4f0ac6708eae524b31d6b
willtsh <91974481+willtsh@users.noreply.github.com>
2021-11-08 15:42:16 -0500

    Update reducers.ts
    fixed french button

--- React/vbw/src/store/reducers.ts
+++ React/vbw/src/store/reducers.ts
@@ -7,12 +7,14 @@ import ru_RU from "../assets/lang/ru_RU.json";
 import fi_FIN from "../assets/lang/fi_FIN.json";
 import de_DE from "../assets/lang/de_DE.json";
 import geo_Geo from "../assets/lang/geo_Geo.json";
+import fre_FRE from "../assets/lang/fre_FRE.json";
 
 const enUS = en_US;
 const ruRU = ru_RU;
 const fiFIN = fi_FIN;
 const deDE = de_DE;
 const geoGeo = geo_Geo;
+const freFRE = fre_FRE;
 
 export interface ApplicatonState {
   settings_open: boolean;
