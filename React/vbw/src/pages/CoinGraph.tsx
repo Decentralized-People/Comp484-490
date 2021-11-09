67be674db089a5ea6d22508d0c504415ebe2c3f3
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2021-11-09 10:05:20 -0500

    Add graph but not finish it

--- React/vbw/src/pages/CoinGraph.tsx
+++ React/vbw/src/pages/CoinGraph.tsx
@@ -7,12 +7,144 @@ import { Coin } from "../store/interfaces";
 import axios, { AxiosResponse } from 'axios';
 import { useDispatch, useStore } from "react-redux";
 import { useTypedSelector } from "../store";
+import { ResponsivePie } from '@nivo/pie'
+
 
 export function CoinGraph(lang: Language): JSX.Element{
     
     const dispatch = useDispatch();
 
     const coinsToPrint: Coin[] = useTypedSelector((state) => state.reducers.coins);
+
+    interface ResponsivePieInterface {
+        id: string;
+        label: string;
+        value: string;
+        color: number;
+    }
+    
+    const graphData: ResponsivePieInterface[] = coinsToPrint.map((coin: Coin) => {
+        return {
+            id: coin.coin,
+            label: coin.coin,
+            value: coin.ratedPower.toString(),
+            color: coin.ratedPower
+        }
+    })
+    
+    // const MyResponsivePie = () => (
+    //     <ResponsivePie
+    //         data={graphData}
+    //         margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+    //         innerRadius={0.5}
+    //         padAngle={0.7}
+    //         cornerRadius={3}
+    //         activeOuterRadiusOffset={8}
+    //         borderWidth={1}
+    //         borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.2 ] ] }}
+    //         arcLinkLabelsSkipAngle={10}
+    //         arcLinkLabelsTextColor="#333333"
+    //         arcLinkLabelsThickness={2}
+    //         arcLinkLabelsColor={{ from: 'color' }}
+    //         arcLabelsSkipAngle={10}
+    //         arcLabelsTextColor={{ from: 'color', modifiers: [ [ 'darker', 2 ] ] }}
+    //         defs={[
+    //             {
+    //                 id: 'dots',
+    //                 type: 'patternDots',
+    //                 background: 'inherit',
+    //                 color: 'rgba(255, 255, 255, 0.3)',
+    //                 size: 4,
+    //                 padding: 1,
+    //                 stagger: true
+    //             },
+    //             {
+    //                 id: 'lines',
+    //                 type: 'patternLines',
+    //                 background: 'inherit',
+    //                 color: 'rgba(255, 255, 255, 0.3)',
+    //                 rotation: -45,
+    //                 lineWidth: 6,
+    //                 spacing: 10
+    //             }
+    //         ]}
+    //         fill={[
+    //             {
+    //                 match: {
+    //                     id: 'ruby'
+    //                 },
+    //                 id: 'dots'
+    //             },
+    //             {
+    //                 match: {
+    //                     id: 'c'
+    //                 },
+    //                 id: 'dots'
+    //             },
+    //             {
+    //                 match: {
+    //                     id: 'go'
+    //                 },
+    //                 id: 'dots'
+    //             },
+    //             {
+    //                 match: {
+    //                     id: 'python'
+    //                 },
+    //                 id: 'dots'
+    //             },
+    //             {
+    //                 match: {
+    //                     id: 'scala'
+    //                 },
+    //                 id: 'lines'
+    //             },
+    //             {
+    //                 match: {
+    //                     id: 'lisp'
+    //                 },
+    //                 id: 'lines'
+    //             },
+    //             {
+    //                 match: {
+    //                     id: 'elixir'
+    //                 },
+    //                 id: 'lines'
+    //             },
+    //             {
+    //                 match: {
+    //                     id: 'javascript'
+    //                 },
+    //                 id: 'lines'
+    //             }
+    //         ]}
+    //         legends={[
+    //             {
+    //                 anchor: 'bottom',
+    //                 direction: 'row',
+    //                 justify: false,
+    //                 translateX: 0,
+    //                 translateY: 56,
+    //                 itemsSpacing: 0,
+    //                 itemWidth: 100,
+    //                 itemHeight: 18,
+    //                 itemTextColor: '#999',
+    //                 itemDirection: 'left-to-right',
+    //                 itemOpacity: 1,
+    //                 symbolSize: 18,
+    //                 symbolShape: 'circle',
+    //                 effects: [
+    //                     {
+    //                         on: 'hover',
+    //                         style: {
+    //                             itemTextColor: '#000'
+    //                         }
+    //                     }
+    //                 ]
+    //             }
+    //         ]}
+    //     />
+    // )
 
     useEffect(() => {
 
@@ -99,11 +231,129 @@ export function CoinGraph(lang: Language): JSX.Element{
         fetchData();
 
     })
+
+    const MyResponsivePie = ( /* see data tab */ ) => (
+        <ResponsivePie
+            data={coinsToPrint}
+            margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+            innerRadius={0.5}
+            padAngle={0.7}
+            cornerRadius={3}
+            activeOuterRadiusOffset={8}
+            borderWidth={1}
+            borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.2 ] ] }}
+            arcLinkLabelsSkipAngle={10}
+            arcLinkLabelsTextColor="#333333"
+            arcLinkLabelsThickness={2}
+            arcLinkLabelsColor={{ from: 'color' }}
+            arcLabelsSkipAngle={10}
+            arcLabelsTextColor={{ from: 'color', modifiers: [ [ 'darker', 2 ] ] }}
+            defs={[
+                {
+                    id: 'dots',
+                    type: 'patternDots',
+                    background: 'inherit',
+                    color: 'rgba(255, 255, 255, 0.3)',
+                    size: 4,
+                    padding: 1,
+                    stagger: true
+                },
+                {
+                    id: 'lines',
+                    type: 'patternLines',
+                    background: 'inherit',
+                    color: 'rgba(255, 255, 255, 0.3)',
+                    rotation: -45,
+                    lineWidth: 6,
+                    spacing: 10
+                }
+            ]}
+            fill={[
+                {
+                    match: {
+                        id: 'ruby'
+                    },
+                    id: 'dots'
+                },
+                {
+                    match: {
+                        id: 'c'
+                    },
+                    id: 'dots'
+                },
+                {
+                    match: {
+                        id: 'go'
+                    },
+                    id: 'dots'
+                },
+                {
+                    match: {
+                        id: 'python'
+                    },
+                    id: 'dots'
+                },
+                {
+                    match: {
+                        id: 'scala'
+                    },
+                    id: 'lines'
+                },
+                {
+                    match: {
+                        id: 'lisp'
+                    },
+                    id: 'lines'
+                },
+                {
+                    match: {
+                        id: 'elixir'
+                    },
+                    id: 'lines'
+                },
+                {
+                    match: {
+                        id: 'javascript'
+                    },
+                    id: 'lines'
+                }
+            ]}
+            legends={[
+                {
+                    anchor: 'bottom',
+                    direction: 'row',
+                    justify: false,
+                    translateX: 0,
+                    translateY: 56,
+                    itemsSpacing: 0,
+                    itemWidth: 100,
+                    itemHeight: 18,
+                    itemTextColor: '#999',
+                    itemDirection: 'left-to-right',
+                    itemOpacity: 1,
+                    symbolSize: 18,
+                    symbolShape: 'circle',
+                    effects: [
+                        {
+                            on: 'hover',
+                            style: {
+                                itemTextColor: '#000'
+                            }
+                        }
+                    ]
+                }
+            ]}
+        />
+    )
+
     return(
         <div>
+            hey?
+            <MyResponsivePie></MyResponsivePie>
             {coinsToPrint.map(coin => {
                 return(
                     <div>
+                        
                         <h5>{coin.coin}</h5>
                         <h5>{coin.algorithm}</h5>
                         <h5>{coin.ratedPower}</h5>
