c8a7d3ff96a975908af30f7d76069bc92fe1a3bd
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2021-11-16 15:25:49 -0500

    Fix the energy's layout

--- React/vbw/src/pages/Energy.tsx
+++ React/vbw/src/pages/Energy.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { Typography, Tabs } from 'antd';
+import { Tabs } from 'antd';
 import './Energy.css'
 import { Language } from "../store/interfaces";
 import { CryptoEnergies } from "./sections/CryptoEnergies";
@@ -10,22 +9,23 @@ import { EnergyConclusion } from "./sections/EnergyConclusion";
 export function Energy(lang: Language){
     return(
         <>
-            <Tabs tabPosition="left" className="Energy-tabs">
-            <Tabs.TabPane tab={lang.side_bar.tab1} key="1">
-                </Tabs.TabPane>
-                <Tabs.TabPane tab={lang.side_bar.tab2} key="2">
-                </Tabs.TabPane>
-                <Tabs.TabPane tab={lang.side_bar.tab3} key="3">
-                </Tabs.TabPane>
-            </Tabs>
-            <Typography.Title level={3} className="Energy-text">
-                {lang.sections.energy.title}
+            <div className="Energy-body-left">
+                <Tabs tabPosition="left" className="Energy-tabs">
+                    <Tabs.TabPane tab={lang.side_bar.tab1} key="1">
+                    </Tabs.TabPane>
+                    <Tabs.TabPane tab={lang.side_bar.tab2} key="2">
+                    </Tabs.TabPane>
+                    <Tabs.TabPane tab={lang.side_bar.tab3} key="3">
+                    </Tabs.TabPane>
+                </Tabs>
+            </div>
+            <div className="Energy-body-right">
                 <EnergyIntro {...lang}> </EnergyIntro>
                 <InfraEnergies {...lang}> </InfraEnergies>
                 <PersonalEnergies {...lang}> </PersonalEnergies>
                 <CryptoEnergies {...lang}></CryptoEnergies>
                 <EnergyConclusion {...lang}></EnergyConclusion>
-            </Typography.Title>
+            </div>
         </>
     )
 }
