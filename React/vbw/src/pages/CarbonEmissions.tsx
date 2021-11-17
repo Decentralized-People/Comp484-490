16afc811ec4d28cea6d6a50fefac08a8f69438fa
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2021-11-17 13:40:43 -0500

    Fix map's border

--- React/vbw/src/pages/CarbonEmissions.tsx
+++ React/vbw/src/pages/CarbonEmissions.tsx
@@ -10,15 +10,17 @@ import { CarbonIntro } from "./sections/CarbonIntro";
 export function CarbonEmissions(lang: Language){
     return(
         <>
-            <Tabs tabPosition="left" className="CarbonEmissions-tabs">
-                <Tabs.TabPane tab={lang.side_bar.tab1} key="1">
-                </Tabs.TabPane>
-                <Tabs.TabPane tab={lang.side_bar.tab2} key="2">
-                </Tabs.TabPane>
-                <Tabs.TabPane tab={lang.side_bar.tab3} key="3">
-                </Tabs.TabPane>
-            </Tabs>
-            <div className="CarbonEmissions-text">
+            <div className="CarbonEmissions-body-left">
+                <Tabs tabPosition="left" className="CarbonEmissions-tabs">
+                    <Tabs.TabPane tab={lang.side_bar.tab1} key="1">
+                    </Tabs.TabPane>
+                    <Tabs.TabPane tab={lang.side_bar.tab2} key="2">
+                    </Tabs.TabPane>
+                    <Tabs.TabPane tab={lang.side_bar.tab3} key="3">
+                    </Tabs.TabPane>
+                </Tabs>
+            </div>
+            <div className="CarbonEmissions-body-right">
                 {lang.sections.carbon_emissions.title}
                 Global CO2 emissions from fossil fuels
                 https://ourworldindata.org/co2-emissions
