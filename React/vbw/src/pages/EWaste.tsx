f1c37a3910d2cd543d8932f19e9523feb87a4899
David Xu <85957886+DaveXFin@users.noreply.github.com>
2021-11-17 15:31:19 -0500

    text
    -added text into the e waste section

--- React/vbw/src/pages/EWaste.tsx
+++ React/vbw/src/pages/EWaste.tsx
@@ -19,6 +19,16 @@ export function EWaste(lang: Language){
             </div>
             <div className="Ewaste-body-right">
                 <RecycleGraph {...lang}> </RecycleGraph>
+               
+                <p className="">E-waste pollution is a matter that is not discussed as much as it is supposed to be. Most of the consumers are not aware of the proper ways to recycle their e-waste. 
+                    Especially with the increasing idea of greenwash, it is not helping the consumers to actually understand the proper ways of handling the e-waste. The amount of e-waste we produce as consumers increases year by year. The big tech companies are the biggest influence in the increasing consumption. It does not help when tech companies are coming out with new technologies every year, many companies are boosting obsolescence in technology. This is a cycle that we consumers need to be more aware of so that we do not need a new “Iphone” every year. Many of these devices chemical leaching which results in hazardous toxins to be released into our environment. When the environment is polluted it
+                </p>
+                <p>Cryptocurrency mining produces large quantities of e-waste with not much attention from the media. Many of these high computational machines end up in the waste without the proper process of recycling the high power machines. Especially due the short life span of these crypto mining rigs with an average of only 1.29 years(bbc news) of life span on these machines. Do keep in mind that these machines are powered on 24/7 for that average life span of 1.29 years. It is important to keep in mind that these crypto mining rigs and graphics cards used to mine crypto currencies can only be used once to mine for the crypto currency. It cannot be used to mine the same crypto currency again. This results in the machine to be considered as an e-waste product. </p>
+            
+                <li>Bitcoin mining producing tonnes of waste - BBC News</li>
+                <li>Cleaning Up Electronic Waste (E-Waste) | US EPA</li>
+                <li>Bitcoin poses major electronic-waste problem (acs.org)</li>
+            
             </div>
         </>
     )
