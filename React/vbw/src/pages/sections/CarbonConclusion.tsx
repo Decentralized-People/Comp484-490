96270bbe4477d10de36e9f9326da53b40e7f64bb
superduperTerry <93404228+superduperTerry@users.noreply.github.com>
2021-11-18 22:08:38 -0500

    Update CarbonConclusion.tsx

--- React/vbw/src/pages/sections/CarbonConclusion.tsx
+++ React/vbw/src/pages/sections/CarbonConclusion.tsx
@@ -23,7 +23,8 @@ export function CarbonConclusion(lang: Language): JSX.Element{
             <div className="CarbonConclusion-FlexContainer">
                 <div className="CarbonConclusion-Description">
                 <h2>Concluding Thoughts</h2>
-                <p>These are my concluding thoughts that I'll update</p>
+                <p>In conclusion it is crucial for us to be more aware of our carbon footprint. It is difficult for us to go completely carbon neutral. One of the best ways we can combat the emission is to offset our carbon footprint. This means we need to find ways to negate the carbon footprint we leave individually. These could be small changes such as turning off lights when not being used. Buying second-hand items instead of buying new ones, especially electronics. Also shopping at companies who care about the environment and use environmentally friendly resources. 
+                </p>
                 </div>
             </div>
         </div>
