c431d3ce31f085fa06055b30d6ef9304f292d9ff
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2021-11-19 16:10:46 -0500

    Upload website

--- React/vbw/src/store/store.ts
+++
@@ -1,8 +0,0 @@
-import { createStore } from "redux";
-import rootReducer from "./index";
-
-export const store = createStore(
-    rootReducer,
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ && 
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-);
