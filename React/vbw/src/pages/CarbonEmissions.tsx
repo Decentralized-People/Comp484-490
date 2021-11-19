09f4d10fa14642e9c0343386d5730887af0104c9
superduperTerry <93404228+superduperTerry@users.noreply.github.com>
2021-11-18 23:57:01 -0500

    Update CarbonEmissions.tsx

--- React/vbw/src/pages/CarbonEmissions.tsx
+++ React/vbw/src/pages/CarbonEmissions.tsx
@@ -24,6 +24,7 @@ export function CarbonEmissions(lang: Language){
                 <div className="CarbonEmissions-body">
                     <CarbonIntro {...lang}></CarbonIntro>
                     <WorldCarbonEmissionsMap {...lang}></WorldCarbonEmissionsMap>
+                    <p>We hear the temperature increases every year and the ocean becomes more acidic. The biggest contribution to these causes is the excess carbon dioxide emission that is produced by us. So this brings the questions: why is it important for us to be aware of our carbon footprint and is carbon itself harmful? To break it into simple terms carbon is one of the most common elements it is everywhere in our body, air, etc. Plants used carbon dioxide to produce oxygen as a part of the cycle that supports life on earth. But what we humans are doing is producing excess carbon into the atmosphere. This can cause extreme weather changes, for example Siberia. It was the first time in 150 years in history. </p>
                     <SiberianGlobalWarming {...lang}></SiberianGlobalWarming>
                     <CarbonConclusion {...lang}></CarbonConclusion>  
                 </div>
